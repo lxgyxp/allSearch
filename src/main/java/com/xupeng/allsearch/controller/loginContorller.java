5bf4d6358ff1b19a915b1070b722640a298c2716
xupeng <xupenga@adtec.com.cn>
2020-01-06 16:48:08 +0800

    html

--- src/main/java/com/xupeng/allsearch/controller/loginContorller.java
+++ src/main/java/com/xupeng/allsearch/controller/loginContorller.java
@@ -1,13 +1,13 @@
 package com.xupeng.allsearch.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class loginContorller {
 
     @RequestMapping(value = "/login")
-    public String login(){
-        return "Hello World!";
+    public String login() {
+        return "/index";
     }
 }
