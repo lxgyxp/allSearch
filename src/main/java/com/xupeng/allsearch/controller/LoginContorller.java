014b1c795313d88d1733180b81f3000cefaec626
xupeng <xupenga@adtec.com.cn>
2020-02-26 10:24:04 +0800

    集成shiro

--- src/main/java/com/xupeng/allsearch/controller/LoginContorller.java
+++ src/main/java/com/xupeng/allsearch/controller/LoginContorller.java
@@ -2,10 +2,16 @@ package com.xupeng.allsearch.controller;
 
 import com.xupeng.allsearch.example.Note;
 import com.xupeng.allsearch.service.NoteService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginContorller {
@@ -13,10 +19,44 @@ public class LoginContorller {
     @Autowired
     private NoteService noteService;
 
-    @RequestMapping(value = "/login")
+
+    @RequestMapping(value = "/info")
     public String login(Model model) {
         Note note = noteService.selectByTitle("1");
         model.addAttribute("test",note.getNotes());
         return "index";
+    }
+
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String login1() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        // 从SecurityUtils里边创建一个 subject
+        Subject subject = SecurityUtils.getSubject();
+        // 在认证提交前准备 token（令牌）
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        // 执行认证登陆
+        try {
+            subject.login(token);
+        } catch (UnknownAccountException uae) {
+            return "未知账户";
+        } catch (IncorrectCredentialsException ice) {
+            return "密码不正确";
+        } catch (LockedAccountException lae) {
+            return "账户已锁定";
+        } catch (ExcessiveAttemptsException eae) {
+            return "用户名或密码错误次数过多";
+        } catch (AuthenticationException ae) {
+            return "用户名或密码不正确！";
+        }
+        if (subject.isAuthenticated()) {
+            return "redirect:/info";
+        } else {
+            token.clear();
+            return "登录失败";
+        }
     }
 }
